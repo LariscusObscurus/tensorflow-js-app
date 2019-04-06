182299babe08383f6dd877d16d3ef51a9676944c
leonhardt <lariscusObscurus@gmail.com>
2019-04-06 23:58:04 +0200

    Send small pictures to api

--- src/components/DrawingView.tsx
+++ src/components/DrawingView.tsx
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import { WithStyles, withStyles } from '@material-ui/styles';
-import { Theme } from '@material-ui/core';
+import { Theme, Paper, Typography, Button } from '@material-ui/core';
 import { checkSheet } from '../CssUtil';
+import { imag } from '@tensorflow/tfjs';
 
 const styles = (theme: Theme) => {
-  return checkSheet({});
+  return checkSheet({
+    paper: {
+      display: 'inline-block'
+    }
+  });
 };
 
-interface IDrawingViewState {}
+type FrameHandler = (
+  frame: HTMLVideoElement,
+  elapsed: number
+) => void | Promise<void>;
+
+interface IDrawingViewState { 
+  data:string;
+}
 
 interface IDrawingViewProps
   extends React.HtmlHTMLAttributes<HTMLDivElement>,
-    WithStyles<typeof styles, true> {
+  WithStyles<typeof styles, true> {
   width: number;
   height: number;
 }
@@ -19,85 +31,143 @@ interface IDrawingViewProps
 interface IPosition {
   offsetX: number;
   offsetY: number;
+  drag: boolean;
 }
 
 class DrawingView extends Component<IDrawingViewProps, IDrawingViewState> {
-  state: IDrawingViewState = {};
+  state: IDrawingViewState = {data: ""};
 
   canvas = React.createRef<HTMLCanvasElement>();
-  context: CanvasRenderingContext2D | null = null;
+  ctx: CanvasRenderingContext2D | null = null;
 
   isPainting = false;
-  prevPos: IPosition = { offsetX: 0, offsetY: 0 };
-  line: { start: IPosition; stop: IPosition }[] = [];
+  lines: IPosition[] = []
+  
 
   async componentDidMount() {
     if (!this.canvas.current) return;
-    this.context = this.canvas.current.getContext('2d');
+    this.ctx = this.canvas.current.getContext('2d');
   }
 
-  componentDidUpdate() {}
+  componentDidUpdate() { }
 
   startPainting({
     nativeEvent,
   }: React.MouseEvent<HTMLCanvasElement, MouseEvent>) {
-    const { offsetX, offsetY } = nativeEvent;
     this.isPainting = true;
-    this.prevPos = { offsetX, offsetY };
+    const { offsetX, offsetY } = nativeEvent;
+    this.addPosition(offsetX, offsetY);
   }
 
   onMouseMove({
     nativeEvent,
   }: React.MouseEvent<HTMLCanvasElement, MouseEvent>) {
-    if (this.isPainting) {
-      const { offsetX, offsetY } = nativeEvent;
-      const offSetData = { offsetX, offsetY };
-      // Set the start and stop position of the paint event.
-      const positionData = {
-        start: { ...this.prevPos },
-        stop: { ...offSetData },
-      };
-      // Add the position to the line array
-      this.line = this.line.concat(positionData);
-      this.paint(this.prevPos, offSetData, '#000');
-    }
+    if (!this.isPainting) return;
+    const { offsetX, offsetY } = nativeEvent;
+    this.addPosition(offsetX, offsetY, true);
+    this.paint();
   }
 
   stopPainting() {
-    if (this.isPainting) {
-      this.isPainting = false;
+    this.isPainting = false;
+  }
+
+  async createImage(): Promise<any> {
+    if (!this.canvas.current) return;
+    if (!this.ctx) return;
+
+    const resizeCanvas = document.createElement('canvas');
+    resizeCanvas.width = 28;
+    resizeCanvas.height= 28;
+    const resizeCanvasCtx =  resizeCanvas.getContext('2d');
+    if (!resizeCanvasCtx) return;
+
+    resizeCanvasCtx.drawImage(this.canvas.current, 0, 0, this.props.width, this.props.height, 0, 0, 28, 28);
+    this.setState({data: resizeCanvas.toDataURL()});
+
+    const data = resizeCanvasCtx.getImageData(0, 0, 28, 28).data;
+
+    const imageArr = [];
+    for (let i = 0; i < data.length; i += 4) {
+        imageArr.push(1- data[i + 3]/ 255) ;
+    }
+    console.log(imageArr);
+    let postData = JSON.stringify({data: [imageArr]});
+    let url = "https://mal2-api.azure-api.net/mnist/score"
+
+    fetch(url, {
+       method: "POST", // *GET, POST, PUT, DELETE, etc.
+       mode: "cors", // no-cors, cors, *same-origin
+       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+       credentials: "same-origin", // include, *same-origin, omit
+       headers: {
+           "Content-Type": "application/json",
+       },
+       redirect: "follow", // manual, *follow, error
+       referrer: "no-referrer", // no-referrer, *client
+       body: postData, // body data type must match "Content-Type" header
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        alert(myJson[0]);
+    });     
+  }
+
+  addPosition(offsetX: number, offsetY: number, drag: boolean = false) {
+    this.lines.push({
+      offsetX,
+      offsetY,
+      drag
+    });
+  }
+
+  paint() {
+    if (!this.ctx) return;
+    this.ctx.clearRect(0, 0, this.props.width, this.props.height);
+
+    this.ctx.lineJoin = "round";
+    this.ctx.lineWidth = 60;
+    this.ctx.strokeStyle = "#010101"
+
+    for(let i=0; i < this.lines.length; i++) {		
+      this.ctx.beginPath();
+      const previous = this.lines[i - 1];
+      const current  = this.lines[i];
+
+      if(current.drag){
+        this.ctx.moveTo(previous.offsetX, previous.offsetY); 
+      }else{
+        // Make short line for a point
+        this.ctx.moveTo(current.offsetX - 1, current.offsetY); 
+      }
+      this. ctx.lineTo(current.offsetX, current.offsetY);
+      this.ctx.closePath();
+      this.ctx.stroke();
     }
   }
 
-  paint(prevPos: IPosition, currPos: IPosition, strokeStyle: string) {
-    if (!this.context) return;
-
-    const { offsetX, offsetY } = currPos;
-    const { offsetX: x, offsetY: y } = prevPos;
-
-    this.context.beginPath();
-    this.context.strokeStyle = strokeStyle;
-    // Move the the prevPosition of the mouse
-    this.context.moveTo(x, y);
-    // Draw a line to the current position of the mouse
-    this.context.lineTo(offsetX, offsetY);
-    // Visualize the line using the strokeStyle
-    this.context.stroke();
-    this.prevPos = { offsetX, offsetY };
-  }
 
   render() {
+    const { classes } = this.props;
     return (
-      <canvas
-        className={this.props.className}
-        ref={this.canvas}
-        width={this.props.width}
-        height={this.props.height}
-        onMouseDown={this.startPainting.bind(this)}
-        onMouseUp={this.stopPainting.bind(this)}
-        onMouseLeave={this.stopPainting.bind(this)}
-        onMouseMove={this.onMouseMove.bind(this)}
-      />
+      <React.Fragment>
+        <Paper className={classes.paper}>
+          <canvas
+            className={this.props.className}
+            ref={this.canvas}
+            width={this.props.width}
+            height={this.props.height}
+            onMouseDown={this.startPainting.bind(this)}
+            onMouseUp={this.stopPainting.bind(this)}
+            onMouseLeave={this.stopPainting.bind(this)}
+            onMouseMove={this.onMouseMove.bind(this)}
+          />
+        </Paper>
+        <Button onClick={this.createImage.bind(this)}>Predict</Button>
+        {this.state.data ? <img src={this.state.data}/> : <div />}
+      </React.Fragment>
     );
   }
 }
